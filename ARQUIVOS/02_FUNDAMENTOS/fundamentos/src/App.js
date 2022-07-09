742f2dfff5d324c031cfa0457c19a7cd0ef6cc8a
ehurafa <rafarfl2010@gmail.com>
2022-07-08 20:28:42 -0400

    my first component

---
+++ ARQUIVOS/02_FUNDAMENTOS/fundamentos/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import FirstComponent from './components/FirstComponent'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos React</h1>
+      <FirstComponent />
+    </div>
+  );
+}
+
+export default App;
