715c6c152e1fd331c4f6d76db626630ec1bc0ea7
ehurafa <rafarfl2010@gmail.com>
2022-07-12 16:23:33 -0400

    comments

--- ARQUIVOS/02_FUNDAMENTOS/fundamentos/src/App.js
+++ ARQUIVOS/02_FUNDAMENTOS/fundamentos/src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
 import FirstComponent from './components/FirstComponent'
 
+import './App.css';
+
+// comment inline
+
 function App() {
+  /* 
+    block comment
+  */
   return (
     <div className="App">
+      { /* comment jsx */ }
       <h1>Fundamentos React</h1>
       <FirstComponent />
     </div>
