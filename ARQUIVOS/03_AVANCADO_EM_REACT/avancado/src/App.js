47133715cd51289354675a59ead5255a9a08dbb9
ehurafa <rafarfl2010@gmail.com>
2022-07-19 20:30:01 -0400

    images public path

---
+++ ARQUIVOS/03_AVANCADO_EM_REACT/avancado/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Avançado em React</h1>
+      <div>
+        <img src="/supra.jpg" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
