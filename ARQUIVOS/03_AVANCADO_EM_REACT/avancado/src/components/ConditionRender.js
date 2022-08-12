c98123accfddd9ff3d884c767337e80b30bcb09b
ehurafa <rafarfl2010@gmail.com>
2022-08-11 21:43:22 -0400

    ternary condition

--- ARQUIVOS/03_AVANCADO_EM_REACT/avancado/src/components/ConditionRender.js
+++ ARQUIVOS/03_AVANCADO_EM_REACT/avancado/src/components/ConditionRender.js
@@ -4,10 +4,28 @@ function ConditionalRender() {
 
     const [x] = useState(true)
 
+    const [name, setName] = useState('Rafael')
+
     return (
         <div>
             <h1>Isso será exibido?</h1>
             {x && <p>Se x for true, sim!</p>}
+
+            {name === 'João' 
+                ? (
+                    <div>
+                        <p>O nome é João</p>
+                    </div>
+                ) 
+                : (
+                    <div>
+                        <p>Nome não encontrado</p>
+                    </div>
+                )
+            }
+
+            <button onClick={() => setName('João')}>Reset name</button>
+
         </div>
     )
 }
