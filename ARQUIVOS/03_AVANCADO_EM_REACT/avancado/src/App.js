1a433fe40f5f72212bc00276f30ddcb3ebf30041
ehurafa <rafarfl2010@gmail.com>
2022-08-17 21:28:44 -0400

    component list rendering

--- ARQUIVOS/03_AVANCADO_EM_REACT/avancado/src/App.js
+++ ARQUIVOS/03_AVANCADO_EM_REACT/avancado/src/App.js
@@ -8,6 +8,13 @@ import ShowUserName from './components/ShowUserName'
 import CarDetails from './components/CarDetails'
 
 function App() {
+
+  const cars = [
+    {id: 1, brand: "Ferrari", color: "Amarelo", newCar: true, km: 0},
+    {id: 2, brand: 'BMW', color: "Preto", newCar: false, km: 5000},
+    {id: 3, brand: 'Toyota', color: "Verde", newCar: false, km: 13921}
+  ]
+
   return (
     <div className="App">
       <h1>Avançado em React</h1>
@@ -32,6 +39,10 @@ function App() {
       <div>
         <CarDetails brand={'Chevrolet'} km={1000} color={'azul'} newCar={true}/>
         <CarDetails brand={'Ford'} km={0} color={'verde'} newCar={false} />
+        {/* loop em array de objetos */}
+        {cars.map((car) => (
+          <CarDetails brand={car.brand} color={car.color} newCar={car.newCar} km={car.km}/>
+        ))}
       </div>
     </div>
   );
