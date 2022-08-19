e56ce3c1b009ff3342e9b52d46d8cb53fe835478
ehurafa <rafarfl2010@gmail.com>
2022-08-19 19:50:49 -0400

    css of component

--- ARQUIVOS/04_CSS_REACT/css/src/App.js
+++ ARQUIVOS/04_CSS_REACT/css/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import MyComponent from './components/MyComponent'
 
 function App() {
   return (
     <div className="App">
       <h1>React com CSS</h1>
+      <MyComponent />
+      <p>Este é um parágrafo do App</p>
     </div>
   );
 }
---
+++ ARQUIVOS/04_CSS_REACT/css/src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import "./MyComponent.css"
+
+function MyComponent() {
+  return (
+    <div>
+        <h1>Css de componente</h1>
+        <p>Este é um parágrafo de componente</p>
+        <p className="my-comp-paragraph">Este também é do componente</p>
+    </div>
+  )
+}
+
+export default MyComponent
