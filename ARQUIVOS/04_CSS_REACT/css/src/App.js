a6290c66ad2420e7434e4d5aeb3c9f3bf343e309
ehurafa <rafarfl2010@gmail.com>
2022-08-19 20:01:08 -0400

    css inline

--- ARQUIVOS/04_CSS_REACT/css/src/App.js
+++ ARQUIVOS/04_CSS_REACT/css/src/App.js
@@ -8,6 +8,11 @@ function App() {
       <h1>React com CSS</h1>
       <MyComponent />
       <p>Este é um parágrafo do App</p>
+      <p style={{
+        color: 'orange',
+        fontSize: '40px',
+        padding: '20px'
+      }}>Esse parágrafo foi estilizado de forma inline</p>
     </div>
   );
 }
