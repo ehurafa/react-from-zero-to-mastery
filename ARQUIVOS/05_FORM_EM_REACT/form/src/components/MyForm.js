a50b139af053636728280d3da5237769351dd6f3
ehurafa <rafarfl2010@gmail.com>
2022-08-29 19:34:57 -0400

    input onchange

--- ARQUIVOS/05_FORM_EM_REACT/form/src/components/MyForm.js
+++ ARQUIVOS/05_FORM_EM_REACT/form/src/components/MyForm.js
@@ -1,13 +1,28 @@
 import './MyForm.css'
+import { useState } from 'react'
 
 function MyForm() {
+
+    // 3 - gerenciamento de dados
+
+    const [name, setName] = useState() 
+    const [email, setEmail] = useState()
+
+
+    const handleName = (e) => {
+        setName(e.target.value)
+    }
+
+    console.log(name)
+
+
     return (
         <div>
             {/* criação de form /*/}
             <form>
                 <div>
                     <label htmlFor="name">Nome:</label>
-                    <input type="text" name="name" placeholder="Digite o seu nome" />
+                    <input type="text" name="name" placeholder="Digite o seu nome" onChange={handleName} />
                     <input type="submit" value="Enviar" />
                 </div>
                 <label>
