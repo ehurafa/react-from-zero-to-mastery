64b2d63eabeca1012228ab989d9562c096b240ef
ehurafa <rafarfl2010@gmail.com>
2022-09-04 21:41:39 -0400

    initial screen

---
+++ ARQUIVOS/6_SECRET_WORD/secretword/src/data/word.js
@@ -0,0 +1,7 @@
+export const wordsList = {
+    carro: ['Motor', 'Porta', 'Capô', 'Pneu', 'Antena'],
+    fruta: ['Banana', 'Maça', 'Pera', 'Mamão', 'Laranja'],
+    corpo: ['Braço', 'Perna', 'Cárebro', 'Pescoço', 'Olhos'],
+    programção: ['Linguagem', 'Framework', 'Javascript', 'React'],
+    alimento: ['Arroz', 'Feijão', 'Carne', 'Leite', 'Ovo']
+}
