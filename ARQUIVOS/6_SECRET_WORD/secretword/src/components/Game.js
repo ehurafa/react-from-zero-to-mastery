197f9d992b0d99df9a4f10230b803a90dac6854b
ehurafa <rafarfl2010@gmail.com>
2022-09-06 10:48:15 -0400

    create the components

---
+++ ARQUIVOS/6_SECRET_WORD/secretword/src/components/Game.js
@@ -0,0 +1,6 @@
+import './Game.css'
+function Game() {
+    return (<div>game</div>);
+}
+
+export default Game;
---
+++ ARQUIVOS/6_SECRET_WORD/secretword/src/components/GameOver.js
@@ -0,0 +1,6 @@
+import './GameOver.css'
+function GameOver() {
+    return (<div>game over</div>);
+}
+
+export default GameOver;
