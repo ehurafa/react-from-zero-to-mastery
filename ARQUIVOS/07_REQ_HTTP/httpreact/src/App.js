cf90b9cef9f0a7c571c666707d7acce93bea1fe6
ehurafa <rafarfl2010@gmail.com>
2022-11-13 20:59:12 -0400

    rescuing data from api

--- ARQUIVOS/07_REQ_HTTP/httpreact/src/App.js
+++ ARQUIVOS/07_REQ_HTTP/httpreact/src/App.js
@@ -1,10 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState, useEffect } from 'react'
+
+const url = 'http://localhost:3000/products'
+
 function App() {
+
+  const [products, setProducts] = useState([])
+
+  // 1 resgatando dados
+  useEffect(() =>{
+
+    async function fetchData() {
+      // const res = fetch(url).then((users) => {
+
+      //  return users.json()
+      // })
+      // setProducts(res)
+      const res = await fetch(url)
+      const data = await res.json()
+      setProducts(data)
+    }
+
+    fetchData()
+
+  }, [])
+
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name} - R$: {product.price}</li>
+        ))}
+      </ul>
     </div>
   );
 }
