4ec442a8ed449da809e3f3517621156d7151e307
ehurafa <rafarfl2010@gmail.com>
2023-01-12 19:36:37 -0400

    passing the user to the context

--- ARQUIVOS/10_MINIBLOG/miniblog/src/App.js
+++ ARQUIVOS/10_MINIBLOG/miniblog/src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { onAuthStateChanged } from 'firebase/auth'
+
+// hooks
+import { useState, useEffect } from 'react'
+import { useAutehtication } from './hooks/useAuthentication';
 
 // context
 import { AuthProvider } from './context/AuthContext'
@@ -16,9 +21,28 @@ import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 
 function App() {
+
+  const [user, setUser] = useState(undefined)
+  const { auth } = useAutehtication()
+
+  const loadingUser = user === undefined
+
+  useEffect(() => {
+
+    onAuthStateChanged(auth, (user) => {
+      setUser(user)
+    })
+
+
+  }, [auth])
+
+  if (loadingUser) {
+    return <p>Carregando...</p>
+  }
+
   return (
     <div className="App">
-      <AuthProvider>
+      <AuthProvider value={ { user } }>
         <BrowserRouter>
           <Navbar />
           <div className="container">
