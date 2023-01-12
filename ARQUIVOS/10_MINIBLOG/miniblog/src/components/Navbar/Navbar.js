2ca69bfd02d2733e52c556534804237028bfea5a
ehurafa <rafarfl2010@gmail.com>
2023-01-12 19:47:20 -0400

    links to authenticated user

--- ARQUIVOS/10_MINIBLOG/miniblog/src/components/Navbar/Navbar.js
+++ ARQUIVOS/10_MINIBLOG/miniblog/src/components/Navbar/Navbar.js
@@ -2,7 +2,12 @@ import styles from './Navbar.module.css'
 
 import { NavLink } from 'react-router-dom'
 
+import { useAutehtication } from '../../hooks/useAuthentication'
+import { useAuthValue } from '../../context/AuthContext'
+
 const Navbar = () => {
+  const { user } = useAuthValue()
+
   return (
     <nav className={styles.navbar}>
         <NavLink to="/" className={styles.brand}>Mini Blog <span>React</span></NavLink>
@@ -10,12 +15,28 @@ const Navbar = () => {
             <li>
                 <NavLink to="/" className={({isActive}) => (isActive ? styles.active : '')}>Home</NavLink>
             </li>
-            <li>
-                <NavLink to="/login" className={({isActive}) => (isActive ? styles.active : '')}>Login</NavLink>
-            </li>
-            <li>
-                <NavLink to="/register" className={({isActive}) => (isActive ? styles.active : '')}>Cadastrar</NavLink>
-            </li>
+
+            {!user && (
+                <>
+                    <li>
+                    <NavLink to="/login" className={({isActive}) => (isActive ? styles.active : '')}>Login</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/register" className={({isActive}) => (isActive ? styles.active : '')}>Cadastrar</NavLink>
+                </li>
+             </>
+            )}
+            {user && (
+                <>
+                    <li>
+                    <NavLink to="/posts/create" className={({isActive}) => (isActive ? styles.active : '')}>Novo post</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/dashboard" className={({isActive}) => (isActive ? styles.active : '')}>Dashboard</NavLink>
+                </li>
+             </>
+            )}
+            
             <li>
                 <NavLink to="/about" className={({isActive}) => (isActive ? styles.active : '')}>Sobre</NavLink>
             </li>
