b347d9ac40e6d65ee7433755253e5ab7b612c599
ehurafa <rafarfl2010@gmail.com>
2023-01-21 20:19:02 -0400

    css of page single post

--- ARQUIVOS/10_MINIBLOG/miniblog/src/pages/Post/Post.js
+++ ARQUIVOS/10_MINIBLOG/miniblog/src/pages/Post/Post.js
@@ -1,4 +1,4 @@
-import { styles } from './Post.module.css'
+import styles from './Post.module.css'
 
 // hooks
 import { useParams } from 'react-router-dom'
@@ -8,10 +8,20 @@ const Post = () => {
     const { id } = useParams()
     const { document: post, loading } = useFetchDocument('posts', id) 
   return (
-    <div>
+    <div className={styles.post_container}>
         {loading && <p>Carregando post...</p>}
         {post && (
-            <h1>{post.title}</h1>
+            <>
+                <h1>{post.title}</h1>
+                <img src={post.image} alt={post.tilte} />
+                <p>{post.body}</p>
+                <h3>Este post trata sobre:</h3>
+                {post.tagsArray.map((tag) => (
+                    <p key={tag}>
+                        <span>#</span>{tag}
+                    </p>
+                ))}
+            </>
         )}
     </div>
   )
