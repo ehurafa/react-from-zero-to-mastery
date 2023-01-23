bb2ba845b7f718907130d10c930b0433fb3ba5a8
ehurafa <rafarfl2010@gmail.com>
2023-01-23 18:26:03 -0400

    finishing dashboard structure

--- ARQUIVOS/10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.js
+++ ARQUIVOS/10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.js
@@ -13,6 +13,14 @@ const Dashboard = () => {
   // posts do usuario
   const { documents: posts, loading } = useFetchDocuments('posts', null, uid)
 
+  const deleteDocument = (id) => {
+
+  }
+
+  if (loading) {
+    return <p>Carregando...</p>
+  }
+
   return (
     <div>
       <h2>Dashboard</h2>
@@ -23,13 +31,27 @@ const Dashboard = () => {
           <Link to="/posts/create" className="btn">Criar primeiro post</Link>
         </div>
       ) : (
-        <div>
-          <p>Tem posts!</p>
-        </div>
+        <>
+          <div>
+            <span>Título</span>
+            <span>Ações</span>
+          </div>
+          { posts && posts.map((post) => (
+            <div key={post.id}>
+              <p>{post.title}</p>
+              <div>
+                <Link to={`/posts/${post.id}`} className="btn btn-outline">Ver</Link>
+                <Link to={`/posts/edit/${post.id}`} className="btn btn-outline">Editar</Link>
+                <button
+                  onClick={() => deleteDocument(post.id)}
+                  className="btn btn-outline btn-danger">
+                    Excluir
+                  </button>
+              </div>
+            </div>
+          ))}
+        </>
       )}
-      { posts && posts.map((post) => (
-        <h3 key={post.uid}>{post.title}</h3>
-      ))}
     </div>
   )
 }
