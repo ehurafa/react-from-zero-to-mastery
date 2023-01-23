57a4335b6c5f14dc74c7de3b1150bb2ab65508ee
ehurafa <rafarfl2010@gmail.com>
2023-01-23 16:51:35 -0400

    show user posts in dashboard

--- ARQUIVOS/10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.js
+++ ARQUIVOS/10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.js
@@ -11,7 +11,7 @@ const Dashboard = () => {
   const uid = user.id
 
   // posts do usuario
-  const posts = []
+  const { documents: posts, loading } = useFetchDocuments('posts', null, uid)
 
   return (
     <div>
@@ -27,6 +27,9 @@ const Dashboard = () => {
           <p>Tem posts!</p>
         </div>
       )}
+      { posts && posts.map((post) => (
+        <h3 key={post.uid}>{post.title}</h3>
+      ))}
     </div>
   )
 }
