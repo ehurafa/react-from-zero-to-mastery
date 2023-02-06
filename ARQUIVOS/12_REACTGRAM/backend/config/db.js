930493d31885a027caa716881de9f624ddce9df8
ehurafa <rafarfl2010@gmail.com>
2023-02-06 19:19:41 -0400

    database connection

--- ARQUIVOS/12_REACTGRAM/backend/config/db.js
+++ ARQUIVOS/12_REACTGRAM/backend/config/db.js
@@ -1,3 +1,23 @@
 const mongoose = require('mongoose')
 
-// connection
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
+
+const conn = async () => {
+
+    try {
+
+        const dbConn = await mongoose.connect(`mongodb+srv://${dbPassword}:${dbPass}@cluster0.49wyv9w.mongodb.net/?retryWrites=true&w=majority`)
+
+        console.log('Conectou ao banco!')
+
+        return dbConn
+
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+conn()
+
+module.exports = conn
