b184aa5969613514273e06b200e6d26afee43e6d
ehurafa <rafarfl2010@gmail.com>
2023-02-20 21:57:35 -0400

    recuing user from authentication

--- ARQUIVOS/12_REACTGRAM/backend/routes/UserRoutes.js
+++ ARQUIVOS/12_REACTGRAM/backend/routes/UserRoutes.js
@@ -4,14 +4,16 @@ const router = express.Router()
 router.use('/api/users', require('./UserRoutes'))
 
 // Controller
-const { register, login } = require('../controllers/UserController')
+const { register, login, getCurrentUser } = require('../controllers/UserController');
 
 // Middlewares
 const validate = require('../middlewares/handleValidation')
 const { userCreateValidation, loginValidation } = require('../middlewares/userValidations')
+const authGuard = require('../middlewares/authGuard');
 
 // Router
 router.post('/register', userCreateValidation(), validate, register)
 router.post('/login', loginValidation(), validate, login)
+router.get('/profile', authGuard, getCurrentUser)
 
 module.exports = router
