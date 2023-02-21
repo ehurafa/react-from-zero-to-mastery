b90514aef236b320ab9b057b5cfe24df3a007bf9
ehurafa <rafarfl2010@gmail.com>
2023-02-21 11:13:02 -0400

    middleware to upload of images

---
+++ ARQUIVOS/12_REACTGRAM/backend/middlewares/imageUpload.js
@@ -0,0 +1,35 @@
+const mlter = require("multer")
+const path = require("path")
+
+// Destination to store image
+const imageStore = multer.diskStorage({
+    destination: (req, file, cb) => {
+        let folder = ""
+
+        if(req.baseUrl.includes("users")) {
+            folder = "users"
+        } else if (req.baseUrl.includes("photos")) {
+            folder = "photos"
+        }
+
+        cd(null, `uploads/${folder}/`)
+    },
+    filename: (req, res, cb) => {
+
+        cb(null, Date.now() + path.extname(file.originalname))
+    }
+})
+
+
+const imageUpload = multer({
+    storage: imageStorage,
+    fileFilter(req, file, cb) {
+        if(!file.originalname.match(/\.(png|jpg)$/)) {
+            // upload only png and jpg formats
+            return cd(new Erro('Por favor, evie apenas png ou jpg!'))
+        }
+        cb(undefined, true)
+    }
+})
+
+module.exports = { imageUpload };
