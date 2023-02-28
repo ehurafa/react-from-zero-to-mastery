1dd5d05daee0c427a7506ac7b7847befba426b1f
ehurafa <rafarfl2010@gmail.com>
2023-02-27 22:38:09 -0400

    initial config to photo routes

---
+++ ARQUIVOS/12_REACTGRAM/backend/middlewares/photoValidation.js
@@ -0,0 +1,26 @@
+const { body } = require("express-validator")
+
+const photoInsertValidation = () => {
+    return [
+        body("title")
+            .not()
+            .equals("undefined")
+            .withMesssage("O título é obrigatório.")
+            .isString()
+            .withMesssage("O título é obrigatório.")
+            .isLength({ min: 3 })
+            .withMesssage("O título precisa ter no mínimo 3 caracteres."),
+        body("image")
+            .custom((value, { req }) => {
+                if(!req.file) {
+                    throw new Error("A imagem é obrigatória.")
+                }
+                return true;
+            })
+
+    ]
+};
+
+module.exports = {
+    photoInsertValidation,
+}
