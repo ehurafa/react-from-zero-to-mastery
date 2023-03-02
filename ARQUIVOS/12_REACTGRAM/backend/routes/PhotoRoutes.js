64f246de45c65e882648c8efd5a77658f4e07913
ehurafa <rafarfl2010@gmail.com>
2023-03-01 21:59:24 -0400

    rescuing all photos

--- ARQUIVOS/12_REACTGRAM/backend/routes/PhotoRoutes.js
+++ ARQUIVOS/12_REACTGRAM/backend/routes/PhotoRoutes.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router();
 
 // Controller
-const { insertPhoto, deletePhoto } = require("../controllers/PhotoController");
+const { insertPhoto, deletePhoto, getAllPhotos } = require("../controllers/PhotoController");
 
 // Middlewares
 const { photoInsertValidation } = require("../middlewares/photoValidation");
@@ -18,6 +18,7 @@ router.post(
     validate,
     insertPhoto
 )
-router.delete("/:id", authGuard, deletePhoto)
+router.delete("/:id", authGuard, deletePhoto);
+router.get("/", authGuard, getAllPhotos)
 
 module.exports = router
