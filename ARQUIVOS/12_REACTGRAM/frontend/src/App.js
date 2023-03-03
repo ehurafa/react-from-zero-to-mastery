42255156438de0f93b42ffd16eb799051c6ee511
ehurafa <rafarfl2010@gmail.com>
2023-03-02 23:06:15 -0400

    navbar component

--- ARQUIVOS/12_REACTGRAM/frontend/src/App.js
+++ ARQUIVOS/12_REACTGRAM/frontend/src/App.js
@@ -3,20 +3,26 @@ import './App.css';
 // Router
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
-// Pges
+// Pages
 import Home from './pages/Home/Home'
 import Register from './pages/Auth/Register';
 import Login from './pages/Auth/Login';
+
+// components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar />
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/login" element={<Login />}/>
           <Route path="/register" element={<Register />}/>
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
