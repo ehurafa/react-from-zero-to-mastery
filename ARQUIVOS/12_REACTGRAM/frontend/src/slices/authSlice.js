14797bf8e46afb47cc1e8f5706617ce228c782ee
ehurafa <rafarfl2010@gmail.com>
2023-03-09 22:41:11 -0400

    login slice

--- ARQUIVOS/12_REACTGRAM/frontend/src/slices/authSlice.js
+++ ARQUIVOS/12_REACTGRAM/frontend/src/slices/authSlice.js
@@ -29,6 +29,21 @@ export const register = createAsyncThunk(
 export const logout = createAsyncThunk("auth/logout", async () => {
   await authService.logout();
 })
+
+// Sign an user
+export const login = createAsyncThunk(
+  "auth/login",
+  async (user, thunkAPI) => {
+    const data = await authService.login(user);
+
+    // Check for errors
+    if (data.errors) {
+      return thunkAPI.rejectWithValue(data.errors[0]);
+    }
+
+    return data;
+  }
+);
   
 
   export const authSlice = createSlice({
@@ -62,6 +77,21 @@ export const logout = createAsyncThunk("auth/logout", async () => {
             state.loading = false;
             state.success = true;
             state.error = null;
+            state.user = null;
+          })
+          .addCase(login.pending, (state) => {
+            state.loading = true;
+            state.error = null;
+          })
+          .addCase(login.fulfilled, (state, action) => {
+            state.loading = false;
+            state.success = true;
+            state.error = null;
+            state.user = action.payload;
+          })
+          .addCase(login.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
             state.user = null;
           })
     }
