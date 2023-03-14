4bdafc7e8a5bec31c16c673adfb07fb6430a03f3
ehurafa <rafarfl2010@gmail.com>
2023-03-13 20:50:15 -0400

    create user reducer

--- ARQUIVOS/12_REACTGRAM/frontend/src/store.js
+++ ARQUIVOS/12_REACTGRAM/frontend/src/store.js
@@ -1,10 +1,12 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 
 import authReducer from './slices/authSlice';
+import userReducer from './slices/userSlice';
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
+        user: userReducer,
     },
     middleware: getDefaultMiddleware({
         serializableCheck: false,
