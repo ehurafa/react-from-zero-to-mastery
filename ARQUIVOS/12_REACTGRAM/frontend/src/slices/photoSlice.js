6618b4a0aa99c73593ddb398aa25c95a85cb82b6
ehurafa <rafarfl2010@gmail.com>
2023-04-27 19:06:37 -0400

    slice to insert photos

--- ARQUIVOS/12_REACTGRAM/frontend/src/slices/photoSlice.js
+++ ARQUIVOS/12_REACTGRAM/frontend/src/slices/photoSlice.js
@@ -10,7 +10,20 @@ const initialState = {
     message: null
 }
 
-// functions
+// publish user photo
+export const publishPhoto = createAsyncThunk(
+    "photo/publish",
+    async(photo, thunkAPI) => {
+        const token = thunkAPI.getState().auth.user.token 
+        const data = await photoService.pusblishPhoto(photo, token) 
+
+        // Check for errors
+        if(data.errors ) {
+            return thunkAPI.rejectWithValue(data.errors[0])
+        }
+        return data
+    }
+)
 
 export const photoSlice = createSlice({
     name: "photo",
@@ -19,7 +32,27 @@ export const photoSlice = createSlice({
         resetMessage: (state) => {
             state.message = null;
         }
-    }
+    },
+    extraReducers: (builder) => {
+        builder
+          .addCase(publishPhoto.pending, (state) => {
+            state.loading = true;
+            state.error = null;
+          })
+          .addCase(publishPhoto.fulfilled, (state, action) => {
+            state.loading = false;
+            state.success = true;
+            state.error = null;
+            state.photo = action.payload;
+            state.photos.unshift(state.photo);
+            state.message = "Foto publicada com sucesso!";
+          })
+          .addCase(publishPhoto.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+            state.photo = {};
+          })
+        }
 })
 
 export const { resetMessage } = photoSlice.actions
