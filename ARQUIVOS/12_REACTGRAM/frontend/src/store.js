80de601574a20b33239eed5d941a312904acfb62
ehurafa <rafarfl2010@gmail.com>
2023-04-26 22:25:19 -0400

    initialize reducer of photos

--- ARQUIVOS/12_REACTGRAM/frontend/src/store.js
+++ ARQUIVOS/12_REACTGRAM/frontend/src/store.js
@@ -2,11 +2,13 @@ import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 
 import authReducer from './slices/authSlice';
 import userReducer from './slices/userSlice';
+import photoReducer from './slices/photoSlice';
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         user: userReducer,
+        photo: photoReducer,
     },
     middleware: getDefaultMiddleware({
         serializableCheck: false,
