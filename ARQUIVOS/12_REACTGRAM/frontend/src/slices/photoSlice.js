ef3da7214b5fda8db1785197fcf193e60de80cc1
ehurafa <rafarfl2010@gmail.com>
2023-05-10 20:54:34 -0400

    photo editing slice

--- ARQUIVOS/12_REACTGRAM/frontend/src/slices/photoSlice.js
+++ ARQUIVOS/12_REACTGRAM/frontend/src/slices/photoSlice.js
@@ -112,8 +112,48 @@ export const photoSlice = createSlice({
             state.error = action.payload;
             state.photo = {};
           })
+          .addCase(updatePhoto.pending, (state) => {
+            state.loading = true;
+            state.error = null;
+          })
+          .addCase(updatePhoto.fulfilled, (state, action) => {
+            state.loading = false;
+            state.success = true;
+            state.error = null;
+
+            state.photos.map((photo) => {
+              if(photo.id === action.payload.photo._id) {
+                return photo.title = action.payload.photo.title
+              }
+              return photo;
+            });
+
+            state.message = action.payload.message
+
+          })
+          .addCase(updatePhoto.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+            state.photo = {};
+          })
         }
 })
+
+// Update a photo
+const updatePhoto = createAsyncThunk(
+  "photo/update",
+  async(photoData, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token
+    const data = await photoService.updatePhoto({ title: photoData.title }, photoData.id, token)
+
+     // Check for errors
+     if(data.errors ) {
+          return thunkAPI.rejectWithValue(data.errors[0])
+      }
+
+    return data
+  }
+)
 
 export const { resetMessage } = photoSlice.actions
 export default photoSlice.reducer
