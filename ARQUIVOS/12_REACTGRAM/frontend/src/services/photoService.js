8d0ccf47ae3164ca57281ca0bf0851a78649f6ac
ehurafa <rafarfl2010@gmail.com>
2023-05-14 11:24:17 -0400

    photo service by id

--- ARQUIVOS/12_REACTGRAM/frontend/src/services/photoService.js
+++ ARQUIVOS/12_REACTGRAM/frontend/src/services/photoService.js
@@ -63,6 +63,27 @@ const updatePhoto = async(data, id , token) => {
 
 }
 
-const photoService = { publishPhoto, getUserPhotos, deletePhoto, updatePhoto }
+// Get photo by id
+const getPhoto = async(id) => {
+    const config = requestConfig("GET");
+
+    try {
+        const res = await fetch(api + "/photos/" + id, config)
+            .then((res) => res.json())
+            .catch((err) => err);
+            return res
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+const photoService = { 
+    publishPhoto,
+    getUserPhotos,
+    deletePhoto,
+    updatePhoto,
+    getPhoto
+}
 
 export default photoService
