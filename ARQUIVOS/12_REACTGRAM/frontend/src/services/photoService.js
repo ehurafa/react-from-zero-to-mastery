54ead0efb416ecc7dbcbc26ae6ae79c4ab526f31
ehurafa <rafarfl2010@gmail.com>
2023-07-02 22:38:39 -0400

    search service

--- ARQUIVOS/12_REACTGRAM/frontend/src/services/photoService.js
+++ ARQUIVOS/12_REACTGRAM/frontend/src/services/photoService.js
@@ -127,6 +127,25 @@ const getPhotos = async(token) => {
     }
 }
 
+// Search photo by title 
+const searchPhotos = async(query, token) => {
+
+    const config = requestConfig("GET", null, token);
+
+    try {
+
+        const res = await fetch(api + "/photos/search?q=" + query, config)
+                    .then((res) => res.json())
+                    .catch((err) => err);
+
+        return res;
+
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
 const photoService = { 
     publishPhoto,
     getUserPhotos,
@@ -135,7 +154,8 @@ const photoService = {
     getPhoto,
     like,
     comment,
-    getPhotos
+    getPhotos,
+    searchPhotos
 }
 
 export default photoService
