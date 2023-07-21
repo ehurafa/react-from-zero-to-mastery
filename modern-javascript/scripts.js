58b67eaf8e054ed09cb021f56c3cfed5851decb2
ehurafa <rafarfl2010@gmail.com>
2023-07-21 19:31:20 -0400

    destructuring

--- modern-javascript/scripts.js
+++ modern-javascript/scripts.js
@@ -90,3 +90,19 @@ const userName = "Rafael"
 const age = 32
 
 console.log(`O nome do usuário é ${userName} e ele tem ${age} anos.`);
+
+// 6 destructuring
+const fruits = ["Maça", "Laranja", "Mamão"];
+
+const [f1, f2, f3] = fruits;
+
+const productDetails = {
+    name: "Mouse",
+    price: 39.99,
+    category: "Periféricos",
+    color: "Cinza"
+}
+
+const { name: productName, price, category: productCategory, color } = productDetails;
+
+console.log(`O nome do produto é ${productName}, custa R$${price}, pertence a categoria ${productCategory}`)
