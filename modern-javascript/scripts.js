13d728dcab988cca8251fe1e257c40b82f0f0092
ehurafa <rafarfl2010@gmail.com>
2023-07-25 18:26:41 -0400

    OO inheritance

--- modern-javascript/scripts.js
+++ modern-javascript/scripts.js
@@ -151,3 +151,28 @@ const tenis = new Product("Tenis verde", 200)
 
 console.log(tenis.productWithDiscount(20))
 
+//9 inheritance
+class ProductWithAttributes extends Product {
+
+    constructor(name, price, colors) {
+        super(name, price)
+        this.colors = colors
+    }
+
+    showColors() {
+        console.log('As cores são:')
+        this.colors.forEach(color => {
+            console.log(color)
+        })
+    }
+
+}
+
+const hat = new ProductWithAttributes('Chapéu', 29.99, ['Preto', 'Azul', 'Verde'])
+
+console.log(hat.name)
+
+console.log(hat.productWithDiscount(10))
+
+hat.showColors()
+
