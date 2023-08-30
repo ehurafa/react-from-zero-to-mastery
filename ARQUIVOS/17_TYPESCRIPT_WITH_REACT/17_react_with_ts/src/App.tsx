e7056ca4130de71a99b408b39e3671228516a27d
ehurafa <rafarfl2010@gmail.com>
2023-08-29 20:47:50 -0400

    consuming the context

--- ARQUIVOS/17_TYPESCRIPT_WITH_REACT/17_react_with_ts/src/App.tsx
+++ ARQUIVOS/17_TYPESCRIPT_WITH_REACT/17_react_with_ts/src/App.tsx
@@ -12,6 +12,9 @@ import Destructuring, { Category } from './components/Destructuring';
 // 6 - useState
 import State from './components/State';
 
+// 10 - utilizando contexto
+import Context from "./components/Context"
+
 // 8 - type
 type textOrNull = string | null;
 
@@ -75,6 +78,7 @@ function App() {
         { mySecondText && (
           <p>tem texto na variável</p>
         )}
+        <Context />
       </div>
    </AppContext.Provider>
   );
