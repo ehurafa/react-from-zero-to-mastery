d2f1d71336e9239f4d90fc104e62f10823156e4c
ehurafa <rafarfl2010@gmail.com>
2023-09-10 22:16:35 -0400

    create the form

--- ARQUIVOS/18_REACT-TODO/react-ts-todo/src/App.tsx
+++ ARQUIVOS/18_REACT-TODO/react-ts-todo/src/App.tsx
@@ -6,13 +6,22 @@ import styles from "./App.module.css";
 // components
 import Header from './components/Header';
 import Footer from './components/Footer';
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
 
 function App() {
   return (
    <div>
     <Header />
      <main className={ styles.main }>
-     <h1>Conteúdo...</h1> 
+      <div>
+        <h2>O que você vai fazer?</h2>
+        <TaskForm btnText="Criar tarefa" />
+      </div>
+      <div>
+        <h2>Suas tarefas:</h2>
+        <TaskList />
+      </div>
      </main>
     <Footer />
    </div>
---
+++ ARQUIVOS/18_REACT-TODO/react-ts-todo/src/components/TaskList.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <div>Lista de tarefas</div>
+  )
+}
+
+export default TaskList
