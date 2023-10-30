12316576017a84088f691b46dd5c4b9332cedfec
ehurafa <rafarfl2010@gmail.com>
2023-10-30 18:46:48 -0400

    creating the routes

--- ARQUIVOS/19_MOVIES_LIB/movies_lib/src/App.jsx
+++ ARQUIVOS/19_MOVIES_LIB/movies_lib/src/App.jsx
@@ -5,9 +5,6 @@ function App() {
   return (
     <>
       <h1>The Movies Lib</h1>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
