6e5f5cdf53d370f12f628c80303c45cb9994d6a7
ehurafa <rafarfl2010@gmail.com>
2023-10-31 15:05:16 -0400

    show router outlet

--- ARQUIVOS/19_MOVIES_LIB/movies_lib/src/App.jsx
+++ ARQUIVOS/19_MOVIES_LIB/movies_lib/src/App.jsx
@@ -1,11 +1,20 @@
+import { Link, Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      <h1>The Movies Lib</h1>
-    </>
+    <div className="App">
+      <nav id="navbar">
+        <h2>
+          <Link to="/">Movies Lib</Link>
+        </h2>
+        <Link to="/movie/1">Movie</Link>
+        <Link to="/search">Search</Link>
+      </nav>
+      <h2>Movies Lib</h2>
+      <Outlet />
+    </div>
   )
 }
 
--- ARQUIVOS/19_MOVIES_LIB/movies_lib/src/main.jsx
+++ ARQUIVOS/19_MOVIES_LIB/movies_lib/src/main.jsx
@@ -14,9 +14,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route element={ <App />}>
-          <Route path="/" element={ <Home /> } >
-          <Route path="/movie/:id" element={ <Movie /> } >
-          <Route path="search" element={ <Search /> } >
+          <Route path="/" element={ <Home /> }  />
+          <Route path="/movie/:id" element={ <Movie /> } />
+          <Route path="search" element={ <Search /> } />
         </Route>
       </Routes>
     </BrowserRouter>
