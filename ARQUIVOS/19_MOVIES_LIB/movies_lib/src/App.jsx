882c62dd915e61742be96b95a82058c404a5c223
ehurafa <rafarfl2010@gmail.com>
2023-11-03 10:47:27 -0400

    create the navibar

--- ARQUIVOS/19_MOVIES_LIB/movies_lib/src/App.jsx
+++ ARQUIVOS/19_MOVIES_LIB/movies_lib/src/App.jsx
@@ -1,17 +1,13 @@
-import { Link, Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import './App.css'
+
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <div className="App">
-      <nav id="navbar">
-        <h2>
-          <Link to="/">Movies Lib</Link>
-        </h2>
-        <Link to="/movie/1">Movie</Link>
-        <Link to="/search">Search</Link>
-      </nav>
+      <Navbar />
       <h2>Movies Lib</h2>
       <Outlet />
     </div>
