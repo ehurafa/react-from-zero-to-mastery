69692e032b146850610ff040ba53fb12e74a9ec7
ehurafa <rafarfl2010@gmail.com>
2023-11-05 13:26:58 -0400

    get data

--- ARQUIVOS/19_MOVIES_LIB/movies_lib/src/pages/Home.jsx
+++ ARQUIVOS/19_MOVIES_LIB/movies_lib/src/pages/Home.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
 
 const Home = () => {
+  const [topMovies, setTopMovies] = useState([]);
+
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    console.log(data);
+  };
+
+  useEffect(() => {
+    const topRatedURL = `${moviesURL}top_rated?${apiKey}`;
+
+    getTopRatedMovies(topRatedURL);
+
+  }, []);
+
   return (
     <div>Home</div>
   )
