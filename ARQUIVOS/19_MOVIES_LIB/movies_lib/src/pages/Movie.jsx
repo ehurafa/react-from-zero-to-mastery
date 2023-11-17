4cb0ed41e0c725537aa03ab40e56b41d54558389
ehurafa <rafarfl2010@gmail.com>
2023-11-17 12:07:09 -0400

    the movie details

--- ARQUIVOS/19_MOVIES_LIB/movies_lib/src/pages/Movie.jsx
+++ ARQUIVOS/19_MOVIES_LIB/movies_lib/src/pages/Movie.jsx
@@ -23,7 +23,14 @@ const Movie = () => {
     const data = await res.json();
 
     setMovie(data);
-  }
+  };
+
+  const formatCurrency = (number) => {
+    return number.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+  };
 
   useEffect(() => {
     const movieUrl = `${ moviesURL}${ id }?${ apiKey }`;
@@ -31,10 +38,37 @@ const Movie = () => {
   }, []);
 
   return (
-    <div>
-      {movie && <>
-        { movie.title }
-      </>}
+    <div className="movie-page">
+      {movie && (
+        <>
+          <MovieCard movie={ movie } showLink={ false } />
+          <p className="tagline">{ movie.tagline }</p>
+          <div className="info">
+            <h3>
+              <BsWallet2 />Orçamento:
+            </h3>
+            <p>{ formatCurrency(movie.budget) }</p>
+          </div>
+          <div className="info">
+            <h3>
+              <BsGraphUp />Receita:
+            </h3>
+            <p>{ formatCurrency(movie.revenue) }</p>
+          </div>
+          <div className="info">
+            <h3>
+              <BsHourglassSplit />Duração:
+            </h3>
+            <p>{ movie.runtime } minutos</p>
+          </div>
+          <div className="info">
+            <h3>
+              <BsFillFileEarmarkTextFill />Descrição:
+            </h3>
+            <p>{ movie.overview } </p>
+          </div>
+        </>
+      )}
     </div>
   )
 }
